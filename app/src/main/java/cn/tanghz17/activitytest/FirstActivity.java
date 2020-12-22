345af899edbd5655ff9d5a92027169b4c2799cb5
TANGhz17 <42084546+TANGhz17@users.noreply.github.com>
2020-12-22 18:48:10 +0800

    1.Toast 2.销毁活动 3.显式intent 4.隐式intent 5.点击按钮打开浏览器 6.点击打开系统拨号界面 7.向下个活动传递数据 8.返回上一活动数据

--- app/src/main/java/cn/tanghz17/activitytest/FirstActivity.java
+++ app/src/main/java/cn/tanghz17/activitytest/FirstActivity.java
@@ -4,7 +4,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -29,18 +31,66 @@ public class FirstActivity extends AppCompatActivity {
                 finish();
                  */
 
+                /*P43 2.3.1---显式intent
                 Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
                 startActivity(intent);
+                */
 
+                /*P45 2.3.2---隐式intent
+                Intent intent = new Intent("cn.tanghz17.activitytest.ACTION_START");
+                intent.addCategory("cn.tanghz17.activitytest.MY_CATEGORY");
+                startActivity(intent);
+                */
+
+                /*P46 2.3.3---点击按钮打开浏览器
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+                */
+
+                /*P49 点击打开系统拨号界面
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+                */
+
+                /*P50 2.3.4---向下个活动传递数据
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                intent.putExtra("extra_data",data);
+                startActivity(intent);
+                */
+
+                /*P51 2.3.5---返回上一活动数据
+                 */
+                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
             }
+
         });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main,menu);
         return true;
     }
+    @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case R.id.add_item:
@@ -55,4 +105,5 @@ public class FirstActivity extends AppCompatActivity {
         }
         return true;
     }
+
 }
---
+++ app/src/main/java/cn/tanghz17/activitytest/ThirdActivity.java
@@ -0,0 +1,15 @@
+package cn.tanghz17.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+
+    }
+}
